8912b4486b04380b46c036f2baf7fafeebbbccf5
Rafi Naufal <152187109+raffnr@users.noreply.github.com>
2023-12-14 11:14:55 +0700

    Update currencies.py

--- currencies.py
+++ currencies.py
@@ -4,12 +4,12 @@ list_currencies = []
 
 host = 'api.frankfurter.app'
 
-# Mengambil data daftar mata uang dari endpoint API
+# Get currencies list from API
 response = requests.get(f'https://{host}/currencies')
 
 currencies = response.json()
 
-# Memasukan data daftar mata uang dari json ke bentuk list
+# Turn json file to list
 for key, value in currencies.items():
     str_curr = f'{key} - {value}'
     list_currencies.append(str_curr)
